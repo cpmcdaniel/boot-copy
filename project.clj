f72a213f09dab51921118ea5b307b1aad2cab772
Craig McDaniel <cpmcdaniel@gmail.com>
2020-04-03 22:27:53 -0400

    project.clj changes

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   "2.0"
   :repositories
   [["clojars" {:url "https://repo.clojars.org/"}]
-   ["maven-central" {:url "https://repo1.maven.org/maven2"}]]
+   ["maven-central" {:url "https://repo1.maven.org/maven2"}]
+   ["clojars"
+    {:url "https://clojars.org/repo/",
+     :username "cpmcdaniel",
+     :password "s4Viorphotocarhorse"}]]
   :dependencies
   [[adzerk/bootlaces "0.2.0" :scope "test"]
    [onetom/boot-lein-generate "0.1.3" :scope "test"]]
